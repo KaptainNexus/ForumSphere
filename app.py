8b6b3c8a2f1781a6d0188179c727b9c12bc8e923
Ukiah Sperry <ukiahsperry@gmail.com>
2024-04-10 22:56:12 -0400

    Ukiahs v0.2 push

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from os import abort
+from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -6,3 +10,17 @@ app = Flask(__name__)
 def index():
     return render_template('pages/index.html')
 
+@app.get('/secret')
+def secret():
+    return render_template('pages/secret.html')
+
+@app.post('/signup')
+def signup():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username or not password:
+        abort (400)
+    existing_user = ...
+    if existing_user is not None:
+        return redirect('/')
+    return 'signup'
