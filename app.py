9d122626f6acda3819bc97db231332f4ed51e59c
Annabella <oav2008@gmail.com>
2024-04-20 10:52:55 -0400

    added new pages

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('pages/index.html')
+    return render_template('new_index.html')
 
 @app.get('/layout')
 def layout():
