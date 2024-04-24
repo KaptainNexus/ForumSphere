322632706e920929238ba71706b093435718e243
Annabella <oav2008@gmail.com>
2024-04-23 20:08:57 -0400

    update database

--- schema.sql
+++ schema.sql
@@ -1,96 +1,11 @@
-CREATE DATABASE final_project;
-
-
-
-
-
-
 
 CREATE DATABASE final_project;
-CREATE TYPE tag AS ENUM ('Beginner', 'Intermediate', 'Advanced', 'Expert', 'Gaming', 'Fintech', 'WEB3', 'Management/Communications', 'Systems',
-'Cyber Security', 'Data Sci/AI/ML', 'Blockchain', 'Metaverse', 'Cloud Computing', 'Networks', 'Operating Systems', 'Hardware/Firmware');
 
-SELECT * FROM Users;
-CREATE TABLE Users
-(
-    user_id    SERIAL PRIMARY KEY,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) NOT NULL
-);
-SELECT * FROM Images;
-CREATE TABLE Images (
-    image_id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP NOT NULL,
-    caption VARCHAR(255),
-    user_id INTEGER NOT NULL,
-    image_link VARCHAR(255) NOT NULL,
-    pinned BOOLEAN NOT NULL DEFAULT FALSE,
-    CONSTRAINT fk_user
-        FOREIGN KEY (user_id)
-        REFERENCES Users(user_id)
-        ON DELETE CASCADE
-
-);
-
-CREATE TABLE Tags
-(
-    tag_id SERIAL PRIMARY KEY,
-    tag_name VARCHAR(255) UNIQUE NOT NULL,
-    tag_type tag
-);
-SELECT * FROM Posts;
-CREATE TABLE Posts
-(
-    post_id    SERIAL PRIMARY KEY,
-    user_id    INTEGER REFERENCES Users(user_id),
-    title      VARCHAR(255) NOT NULL,
-    content    TEXT NOT NULL,
-    last_modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    image_id INTEGER REFERENCES Images(image_id),
-    tag_id INTEGER REFERENCES Tags(tag_id),
-    difficulty INTEGER NOT NULL
-);
-
-CREATE TABLE Comments
-(
-    comment_id SERIAL PRIMARY KEY,
-    post_id    INTEGER REFERENCES Posts(post_id),
-    user_id    INTEGER REFERENCES Users(user_id),
-    content    TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Likes
-(
-    like_id SERIAL PRIMARY KEY,
-    post_id INTEGER REFERENCES Posts(post_id),
-    user_id INTEGER REFERENCES Users(user_id)
-);
-
-CREATE TABLE Follows
-(
-    follow_id SERIAL PRIMARY KEY,
-    follower_id INTEGER REFERENCES Users(user_id),
-    followee_id INTEGER REFERENCES Users(user_id)
-);
-
-CREATE TYPE tag AS ENUM ('Beginner', 'Intermediate', 'Advanced', 'Expert', 'Gaming', 'Fintech', 'WEB3', 'Managment/Communications', 'Systems',
-'Cyber Security', 'Data Sci/AI/ML', 'Blockchain','Metaverse', 'Cloud Computing', 'Networks', 'Operating Systems', 'Hardware/Firmware');
-
-
-
-CREATE TABLE Friendship
-(
-    friendship_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES Users(user_id),
-    friend_id INTEGER REFERENCES Users(user_id)
-);
-
-CREATE TABLE PostTags
-(
-    post_id INTEGER REFERENCES Posts(post_id),
-    tag_id INTEGER REFERENCES Tags(tag_id),
-    PRIMARY KEY (post_id, tag_id)
+CREATE TABLE "User" (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    registration_day TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_login_day TIMESTAMP NOT NULL DEFAULT NOW()
 );
