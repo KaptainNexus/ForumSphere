50bf8ff5353d351bc95e2c40c2b22b615fce93bd
Ty Nixon <kaptainnexus@gmail.com>
2024-04-28 20:43:59 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,39 @@
 
 CREATE DATABASE final_project;
 
-CREATE TABLE "User" (
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    registration_day TIMESTAMP NOT NULL DEFAULT NOW(),
-    last_login_day TIMESTAMP NOT NULL DEFAULT NOW()
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE images (
+    image_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id),
+    image_url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE followers (
+    follower_id SERIAL PRIMARY KEY,
+    follower_user_id INT REFERENCES users(user_id),
+    following_user_id INT REFERENCES users(user_id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id),
+    user_id INT REFERENCES users(user_id),
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
