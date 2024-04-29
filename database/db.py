0839bae4824f5e649e57a3b5f3ea53542dd289cd
Ty Nixon <kaptainnexus@gmail.com>
2024-04-28 20:43:40 -0400

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,12 +1,30 @@
 import os
-from psycopg_pool import ConnectionPool
+import psycopg2
+from psycopg2 import pool
 
 pool = None
 
 def get_pool():
-    global pool
-    if pool is None:
-        pool = ConnectionPool(
-            conninfo=os.getenv('DB_CONNECTION'),
-        )
-    return pool()
+    database = 'forumsphere'
+    user = 'postgres'
+    password = 'Grad2024!'
+    host = 'localhost'
+    port = '5432'
+
+    try:
+        global pool
+        pool = psycopg2.pool.SimpleConnectionPool(1, 10, database=database, user=user, password=password, host=host, port=port)
+        return pool
+    except Exception as e:
+        print(f"Error creating connection pool: {e}")
+        return None
+
+def get_connection():
+    try:
+        if pool is None:
+            get_pool()
+        conn = pool.getconn()  # Corrected line
+        return conn
+    except Exception as e:
+        print(f"Error retrieving connection: {e}")
+        return None
